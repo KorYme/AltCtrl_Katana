a596b377fb0935cac8cb3282e5163779fceae010
Maxens1011 <MaxensThiam@outlook.com>
2024-10-16 09:37:41 +0200

    [PROG] [ADD] InputsBindings
    :D

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,19 +1,69 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public enum ActionType
 {
-    Feint,
+    Sheath,
     Attack,
+    Crit,
     Counter
 }
 
 public class InputManager : MonoBehaviour
 {
+
+    [Serializable]
+    public class InputBinding
+    {
+        public InputActionReference actionRef;
+        [Range(0, 1)]
+        public int playerID;
+        public ActionType bindType;
+
+        public Action<int, ActionType, bool> OnInputBind;
+
+
+        public void BindStartInput(InputAction.CallbackContext ctx)
+        {
+            OnInputBind?.Invoke(playerID, bindType, true);
+        }
+        public void BindCancelInput(InputAction.CallbackContext ctx)
+        {
+            OnInputBind?.Invoke(playerID, bindType, false);
+        }
+    }
+
     public Action<int, ActionType> OnPlayerActionInput;
-    
+
+    #region SwordInputs
+
+    [SerializeField] List<InputBinding> _bindings;
+
+    #endregion
+
+    private void OnEnable()
+    {
+        foreach (InputBinding binding in _bindings)
+        {
+            binding.actionRef.action.started += binding.BindStartInput;
+            binding.actionRef.action.canceled += binding.BindCancelInput;
+            binding.OnInputBind += UpdateInputs;
+        }
+        // set the action's performed as a method to update the input;
+    }
+
+    private void OnDisable()
+    {
+        foreach (InputBinding binding in _bindings)
+        {
+            binding.actionRef.action.started -= binding.BindStartInput;
+            binding.actionRef.action.canceled -= binding.BindCancelInput;
+            binding.OnInputBind -= UpdateInputs;
+        }
+    }
+
     private void Awake()
     {
         if (GameManager.InputManager != null)
@@ -21,5 +71,19 @@ public class InputManager : MonoBehaviour
             Destroy(this);
         }
         GameManager.InputManager = this;
+
+        SetupInputs();
     }
+
+    private void UpdateInputs(int playerIndex, ActionType type, bool uncovered)
+    {
+
+    }
+
+    private void SetupInputs()
+    {
+
+    }
+
+
 }
