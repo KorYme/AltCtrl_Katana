7c91f437fe09f49ae0a66b5d89977152fb040198
KorYme <92892240+KorYme@users.noreply.github.com>
2024-10-23 19:25:11 +0200

    [PROG] [EDIT] : FlashScreen + ReadyCountTimer

--- Assets/Scripts/UI/ReadyCountTimer.cs
+++ Assets/Scripts/UI/ReadyCountTimer.cs
@@ -10,34 +10,19 @@ public class ReadyCountTimer : MonoBehaviour
 
     private void Start()
     {
-        InstanceManager.UIManager.OnDuelStarted += DisplayReady;
         InstanceManager.UIManager.OnDuelTriggered += DisplayGo;
     }
     
     private void OnDestroy()
     {
-        InstanceManager.UIManager.OnDuelStarted -= DisplayReady;
         InstanceManager.UIManager.OnDuelTriggered -= DisplayGo;
-    }
-
-    private void DisplayReady()
-    {
-        // _textDisplay.color = Color.white;
-        // _textDisplay.text = "Ready ?";
     }
 
     private void DisplayGo()
     {
-        _textDisplay.color = Color.white;
         _textDisplay.text = "GO";
         DOTween.Sequence()
             .Append(_textDisplay.DOFade(1, .2f).SetEase(_fadeIn)).Join(_textDisplay.transform.DOScale(1, .2f).SetEase(_fadeIn))
             .Play();
     }
-    
-    private void HideGo()
-    {
-        _textDisplay.color = Color.clear;
-    }
-    
 }
