423ebc3cfefafb70258d6928b0a90455e00c91d1
Maxens1011 <MaxensThiam@outlook.com>
2024-10-24 15:15:37 +0200

    [PROG] [EDIT] PlayersBow to retry

--- Assets/Scripts/UI/RetryAction.cs
+++ Assets/Scripts/UI/RetryAction.cs
@@ -7,17 +7,30 @@ public class RetryAction : MonoBehaviour
 {
     [SerializeField] float _returnToMenuLag;
 
+    private Coroutine _returnCoroutine;
     private void OnEnable()
     {
         InstanceManager.UIManager.OnDuelFinished += StartGameOptions;
+        InstanceManager.JoyconManager.OnPlayersBow += Retry;
     }
 
-    private void StartGameOptions() => StartCoroutine(QueueReturnToMenu());
+    private void StartGameOptions(RoundResult nah) => _returnCoroutine = StartCoroutine(QueueReturnToMenu());
 
+    private void Retry()
+    {
+        if (_returnCoroutine != null)
+        {
+            StopCoroutine(_returnCoroutine);
+            _returnCoroutine = null;
+        }
+        Debug.Log("RETRYING");
+        InstanceManager.GameManager.LoadGamemodeScene(0);
+    }
     private IEnumerator QueueReturnToMenu()
     {
         InstanceManager.UIManager.OnShowRetryActionRequest?.Invoke(true);
         yield return new WaitForSeconds(_returnToMenuLag);
+        Debug.Log("Returning to menu");
         InstanceManager.UIManager.OnReturnToMenuRequest?.Invoke();
     }
 }
